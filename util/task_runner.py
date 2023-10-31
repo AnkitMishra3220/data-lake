a6ea234f7a270523c456deed49d9327d0f0f350a
ankitmishra <ankitmishra3220@gmail.com>
2023-10-31 21:53:43 +0100

    Executor

--- util/task_runner.py
+++ util/task_runner.py
@@ -1,3 +1,7 @@
+import schedule
+import time
+
+
 class Task:
     def __init__(self, name, func, priority):
         self.name = name
@@ -6,8 +10,9 @@ class Task:
 
 
 class Executor:
-    def __init__(self):
+    def __init__(self, start_time):
         self.tasks = []
+        self.start_time = start_time
 
     def add_task(self, task):
         self.tasks.append(task)
@@ -16,7 +21,13 @@ class Executor:
         sorted_task = sorted(self.tasks, key=lambda x: x.priority)
         for task in sorted_task:
             print("Running Task: {}".format(task.name))
-            task.func
+            task.func()
+
+    def start_execution(self):
+        schedule.every().day.at(self.start_time).do(self.run)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
 
 
 if __name__ == '__main__':
@@ -32,11 +43,11 @@ if __name__ == '__main__':
         print("Task 3 Executed")
 
 
-    task_1 = Task("Task_1", task1(), priority=3)
-    task_2 = Task("Task_2", task2(), priority=2)
-    task_3 = Task("Task_3", task3(), priority=1)
-    executor = Executor()
+    task_1 = Task("Task_1", task1, priority=3)
+    task_2 = Task("Task_2", task2, priority=2)
+    task_3 = Task("Task_3", task3, priority=1)
+    executor = Executor("21:53")
     executor.add_task(task_1)
     executor.add_task(task_2)
     executor.add_task(task_3)
-    executor.run()
+    executor.start_execution()
